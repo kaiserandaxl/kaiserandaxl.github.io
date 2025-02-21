74d5c9cd8e2fb63335653e7af9ad0e605e417231
kaiserandaxl <122803145+kaiserandaxl@users.noreply.github.com>
2025-02-21 14:33:58 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,10 +1,2 @@
-var btn = document.getElementById("newsBtn");
 
-var news = ['a','b','c','d','e',];
-
-function newNews (e) {
-    alert("Touched")
-}
-
-btn.addEventListener("click", newNews);
-btn.addEventListener("touchstart", newNews);
+document.getElementById("newsDisplay").innerHTML = "Hello World!"
