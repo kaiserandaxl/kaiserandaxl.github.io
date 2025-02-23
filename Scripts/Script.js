e685e35de88964bf457131863caf59870ee618b5
kaiserandaxl <122803145+kaiserandaxl@users.noreply.github.com>
2025-02-23 14:55:50 -0600

    V1.1 Register
    Code optimization
    
    New Features

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,106 +1,5 @@
-const Menu = {
-    HamburgerBasket: {
-        Price: 8,
-        Name: "Hamburger Basket",
-        UID: "HB"
-    },
-
-    HotDogBasket: {
-        Price: 7,
-        Name: "Hot Dog Basket",
-        UID: "HDB"
-    },
-
-    NachoBoth: {
-        Price: 5,
-        Name: "Nachos Cheese & Chili",
-        UID: "NB"
-    },
-
-    NachoOne: {
-        Price: 4,
-        Name: "Nachos Cheese or Chili",
-        UID: "NO"
-    },
-
-    Hamburger: {
-        Price: 5,
-        Name: "Hamburger",
-        UID: "Ham"
-    },
-
-    HotDogTop: {
-        Price: 5,
-        Name: "Hotdog Cheese &/or Chili",
-        UID: "HDT"
-    },
-
-    HotDog: {
-        Price: 4,
-        Name: "Hotdog Plain",
-        UID: "HD"
-    },
-
-    FritoPie: {
-        Price: 5,
-        Name: "Frito Chili Pie",
-        UID: "FCP"
-    },
-
-    Candy: {
-        Price: 2,
-        Name: "Candy",
-        UID: "Candy"
-    },
-
-    Chips: {
-        Price: 2,
-        Name: "Chips",
-        UID: "Chips"
-    },
-
-    SodaGaterade: {
-        Price: 2,
-        Name: "Soda/Gatorade",
-        UID: "SG"
-    },
-
-    Pickle: {
-        Price: 1,
-        Name: "Pickle",
-        UID: "Pickle"
-    },
-
-    Water: {
-        Price: 1,
-        Name: "Water",
-        UID: "Water"
-    },
-
-    HotCocoa: {
-        Price: 1,
-        Name: "Hot Cocoa",
-        UID: "HC"
-    },
-
-    Coffee: {
-        Price: 1,
-        Name: "Coffee",
-        UID: "Coffee"
-    },
-
-    Popcorn: {
-        Price: 1,
-        Name: "Popcorn",
-        UID: "Popcorn"
-    },
-
-    Cheese: {
-        Price: 1,
-        Name: "Cheese",
-        UID: "Cheese"
-    }
-};
+import { Menu } from "./Menu.js";
+import { Colors } from "/Styles/Colors.js"
 
 const APR = 4;
 const SumLabel = document.getElementById("Total");
@@ -111,36 +10,44 @@ const History = document.getElementById("History")
 var Total = 0;
 var AmountGiven = 0;
 var Register = {};
+var LastSelected = null;
 
 const UpdateTotals = function() {
+    if (Number.isInteger(Total)) {
+        SumLabel.innerHTML = `$${Total}.00`
+    } else {
+        SumLabel.innerHTML = `$${Total}0`
+    }
+
+    ChangeLabel.innerHTML = "$0.00"
+
     if (AmountGiven > 0) {
         var Diff = AmountGiven-Total
-
-        if (Number.isInteger(Total)) {
-            SumLabel.innerHTML = `$${Total}.00`
-        } else {
-            SumLabel.innerHTML = `$${Total}0`
-        }
-
+    
         if (Number.isInteger(Diff)) {
             ChangeLabel.innerHTML = `$${Diff}.00`
         } else {
             ChangeLabel.innerHTML = `$${Diff}0`
         }
-    } else {
-        if (Number.isInteger(Total)) {
-            SumLabel.innerHTML = `$${Total}.00`
-        } else {
-            SumLabel.innerHTML = `$${Total}0`
-        }
+    }
+}
 
-        ChangeLabel.innerHTML = "$0.00"
+const NewHistory = function(Text, Price) {
+    const Button = document.createElement('button');
+
+    Button.innerHTML = Text
+    History.appendChild(Button)
+
+    Button.onclick = function() {
+        Total -= Number(Price)
+        UpdateTotals()
+
+        History.removeChild(Button)
     }
 }
 
 Register.Init = function() {
     var i = 0;
-    var UIDS = {};
 
     for (const [Item, Data] of Object.entries(Menu)) {
         const Button = document.createElement('button');
@@ -153,51 +60,66 @@ Register.Init = function() {
 
         Button.style.left = (175*(i%APR))+(i%APR*5)+9
         Button.style.top = (100*(Math.floor(i/APR)))+(Math.floor(i/APR)*5)+9
+        //Button.style.backgroundColor = Data.Color
 
         document.body.appendChild(Button);
 
-        UIDS[i] = Data.UID
+        Button.onclick = function() {
+            Total += Number(Button.getAttribute("Price"));
+            LastSelected = Button
+
+            NewHistory(`+ ${Button.getAttribute("Name")}`, Button.getAttribute("Price"))
+            UpdateTotals();
+        }
+        
         i += 1
     }
 
     document.getElementById("Cheese").innerHTML = `Cheese</br>50¢`
     document.getElementById("Cheese").setAttribute("Price",0.50)
 
-    for (const [index, UID] of Object.entries(UIDS)) {
-        const Button = document.getElementById(UID)
+    const Filled = ((APR*Math.ceil(i/APR)-1)-(i-1))
 
-        const Clicked = function() {
-            Total += Number(Button.getAttribute("Price"));
+    for (let b = 1; b <= Filled; b++) {
+        const Button = document.createElement('button');
 
-            History.innerHTML += `+ ${Button.getAttribute("Name")}</br>`
+        Button.innerHTML = `${b+1}x`;
+        Button.className = "MenuItem";
 
-            UpdateTotals();
+        Button.style.left = (175*(i%APR))+(i%APR*5)+9
+        Button.style.top = (100*(Math.floor(i/APR)))+(Math.floor(i/APR)*5)+9
+
+        document.body.appendChild(Button);
+
+        Button.onclick = function() {
+            if (LastSelected) {
+                for (let t = 1; t <= b; t++) {
+                    Total += Number(LastSelected.getAttribute("Price"));
+
+                    NewHistory(`+ ${LastSelected.getAttribute("Name")}`, LastSelected.getAttribute("Price"))
+                    UpdateTotals();
+                }
+            }
         }
 
-        Button.onclick = Clicked
+        i += 1
     }
 
     for (const button of document.getElementsByName("CalcButton")) {
-        const clicked = function() {
+        button.onclick = function() {
             AmountGiven += Number(button.innerHTML)
             CalcLabel.innerHTML = AmountGiven
 
             UpdateTotals()
         }
-
-        button.onclick = clicked
     }
 
-    {
-        const Clear = function() {
-            Total = 0;
-            AmountGiven = 0;
-            CalcLabel.innerHTML = 0;
-            History.innerHTML = "";+
-            UpdateTotals()
-        }
-
-        document.getElementById("Clear").onclick = Clear
+    document.getElementById("Clear").onclick = function() {
+        Total = 0;
+        AmountGiven = 0;
+        CalcLabel.innerHTML = 0;
+        History.innerHTML = null
+        UpdateTotals()
     }
 }
 
